b71f2c35c349bb4bff20fea3199bf53c255861c6
PhoenixNest <XenOmeGA@163.com>
2021-12-16 14:48:02 +0800

    Update DuelLinkList.c

--- DS-Type-Link/DuelLinkList/DuelLinkList.c
+++ DS-Type-Link/DuelLinkList/DuelLinkList.c
@@ -20,6 +20,7 @@ Status InsertBefore_DuL(DuelLinkListNode *p, DuelLinkListNode *q) {
     return OK;
 }
 
+// 删除 指针p 所指向的结点，并用 参数e 返回 p结点 的元素值
 Status Delete_DuL(DuelLinkListNode *p, ElemType elem) {
     if (p == NULL || p->prior == NULL) {
         return ERROR;
